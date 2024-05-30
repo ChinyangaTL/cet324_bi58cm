6481138fb0dea68cba1e7cd247b631d6e5dc2ac9
Les Chinyanga <les.chinyanga@gmail.com>
2024-05-31 01:53:22 +0200

    feat(auth): add logout button to home screen

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -1,8 +1,14 @@
 import { auth } from "@/auth";
+import LogoutButton from "@/components/logout-button";
 
 const HomePage = async () => {
   const session = await auth();
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <LogoutButton />
+    </div>
+  );
 };
 
 export default HomePage;
