32a96cc90eb9e9fa116ad2ae12f952e6662e6742
Les Chinyanga <les.chinyanga@gmail.com>
2024-05-30 23:30:35 +0200

    feat(auth-forms): style dark mode auth login form

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,11 @@
+import LoginForm from "@/components/login-form";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex items-center justify-center h-full">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
