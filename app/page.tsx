d87748665fe44db94673e8ba429f264ec919a02c
Les Chinyanga <les.chinyanga@gmail.com>
2024-05-31 19:54:27 +0200

    feat(2fa): two factor model and toekn getters

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { ModeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>hello</p>
+      <h1 className="text-4xl">Welcome To Cyber Locket</h1>
+      <Link
+        href="/auth/login"
+        className={buttonVariants({
+          className: "w-[100px]",
+        })}
+      >
+        Login
+      </Link>
       <ModeToggle />
     </main>
   );
