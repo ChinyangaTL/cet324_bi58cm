91dc40e50788d3f9284f3b063731a68b06156aac
Les Chinyanga <les.chinyanga@gmail.com>
2024-05-31 02:13:03 +0200

    info: leave message on home page

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -1,6 +1,12 @@
 import { auth } from "@/auth";
 import LogoutButton from "@/components/logout-button";
 
+// TODO: CIRCLE BACK TO THIS ERROR (ONLY SEEN IN PROD)
+// ⨯ [Error: An error occurred in the Server Components render. The specific message is omitted in production builds to avoid leaking sensitive details. A digest property is included on this error instance which may provide additional details about the nature of the error.] {
+//   digest: 'DYNAMIC_SERVER_USAGE',
+//   page: '/home'
+// }
+
 const HomePage = async () => {
   const session = await auth();
   return (
